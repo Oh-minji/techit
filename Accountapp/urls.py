27161a69b41f8d9f9562614f9fd95605610f8cff
minjioh <dhalswl94@naver.com>
2023-09-04 15:15:10 +0900

    design update Commit

--- Accountapp/urls.py
+++ Accountapp/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from Accountapp.views import hello_world
+from Accountapp.views import hello_world, AccountCreateView, AccountLoginView, AccountLogoutView
 
 app_name = "accountapp"
 
 
 urlpatterns = [
-    path("hello_world/", hello_world, name= "hello_world")
+    path("hello_world/", hello_world, name= "hello_world"),
 
+    path("create/", AccountCreateView.as_view(), name = "create" ),
+
+    path("login/", AccountLoginView.as_view(), name = "login"),
+
+    path("logout/", AccountLogoutView.as_view(), name = "logout")
 ]
--- Accountapp/views.py
+++ Accountapp/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import base
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from Accountapp.models import Registration
 
@@ -25,3 +29,15 @@ def hello_world(request):
     return render(request, "accountapp/Hello_World.html",
                   context = {"temp": temp,
                              "reg_all" : reg_all})
+
+class AccountCreateView(CreateView):
+        model = User
+        form_class = UserCreationForm
+        success_url = reverse_lazy("accountapp:hello_world")
+        template_name = "accountapp/create.html"
+
+class AccountLoginView(LoginView):
+    template_name = "accountapp/login.html"
+
+class AccountLogoutView(LogoutView):
+    pass
