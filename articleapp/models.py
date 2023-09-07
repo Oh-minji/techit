28c62f9be9811ca2a6648f7229b1ec2632563b45
minjioh <dhalswl94@naver.com>
2023-09-07 14:14:17 +0900

    articleapp create update Commit

---
+++ articleapp/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.template.defaultfilters import title
+
+
+# Create your models here.
+
+class Article(models.Model):
+    image = models.ImageField(upload_to='article/', null=False)
+    title = models.CharField(max_length=255, null=False)
+    content = models.TextField(null=True)
+
+    like = models.IntegerField(default=0)
+
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
