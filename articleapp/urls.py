7c7e7b9d312678f22afd051303fada76a3a3f7e6
minjioh <dhalswl94@naver.com>
2023-09-07 11:44:03 +0900

    articleapp tempview  update Commit

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
 
+from articleapp.views import TempView
 
 app_name = 'articleapp'
 
-urlpatterns = {}
+urlpatterns = {
+    path('temp/', TempView.as_view(), name = 'temp')
+}
