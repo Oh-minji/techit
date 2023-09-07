444c5611768c2dd72820d5e4c40d9a354584465a
minjioh <dhalswl94@naver.com>
2023-09-07 10:35:56 +0900

    profile create rout update Commit

---
+++ profileapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView
+
+from profileapp.forms import ProfileForm
+from profileapp.models import Profile
+
+
+# Create your views here.
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',
+                       kwargs = {'pk':self.request.user.pk})
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    context_object_name = 'target_profile'
+    template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',
+                       kwargs={'pk': self.request.user.pk})
