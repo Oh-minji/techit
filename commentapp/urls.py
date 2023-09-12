0316ceb04889a2a58d0eaffd0a28620c2e807516
minjioh <dhalswl94@naver.com>
2023-09-12 15:16:19 +0900

    mobile update Commit

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView, CommentUpdateView
+from commentapp.views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
 app_name = "commentapp"
-
-
-class CommentUPdateView:
-    pass
 
 
 urlpatterns = [
     path("create/", CommentCreateView.as_view(), name="create"),
     path("update/<int:pk>", CommentUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>", CommentDeleteView.as_view(), name="delete"),
 ]
